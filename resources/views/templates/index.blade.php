978c61e817ff2437475759027c5d167876dbb81c
Xiaolin Jin Lin <x140l1n@hotmail.com>
2022-03-15 13:38:56 +0100

    Add favicon.

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/ico">
     <link rel="stylesheet" href="{{ asset('/assets/css/app.css') }}">
 </head>
 
