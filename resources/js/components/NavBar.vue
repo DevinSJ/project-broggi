1cb064ba8b284f14d7fa4d230e8b087ac7993e6e
GuillemRubioVaron-EXAMEN <guillemrubiovaron@gmail.com>
2022-03-22 11:07:10 +0100

    WIP eliminados comentariso inecesarios de la navbar

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -1,63 +1,5 @@
 <template>
-    <!--- <div>
-    <b-navbar
-      toggleable="lg"
-      type="light"
-      variant="light"
-      class="fondoBackground"
-    >
-      <b-navbar-brand href="/">
-        <img src="/assets/img/logo.svg" width="120" class="ml-2" />
-      </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="w-100 justify-content-center " >
-
-
-          <b-nav-item class="mx-2 link" to="expedients"
-            ><i class="fa-solid fa-folder-open mr-2"></i>Expedients</b-nav-item
-          >
-          <b-nav-item class="mx-2 link " to="trucades"
-            ><i class="fa-solid fa-phone mr-2"></i>Trucades</b-nav-item
-          >
-          <b-nav-item class="mx-2 link" to="grafics"
-            ><i class="fa-solid fa-chart-pie mr-2"></i>Gràfics</b-nav-item
-          >
-          <b-nav-item class="mx-2 link" to="usuaris"
-            ><i class="fa-solid fa-user mr-2"></i>Usuaris</b-nav-item
-          >
-          <b-nav-item-dropdown class="mx-2 link">
-            <template #button-content>
-              <span class="mr-2">Paràmetres</span>
-            </template>
-            <b-dropdown-item href="/tipus-incidents"
-              ><i class="fa-solid fa-list mr-2"></i>Tipus de
-              incidents</b-dropdown-item
-            >
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-
-
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown class="ml-2" right>
-            <template #button-content>
-              <em class="mr-2">{{ user.cognoms }}, {{ user.nom }}</em>
-            </template>
-            <b-dropdown-item href="#"
-              ><i class="fa-solid fa-address-card mr-2"></i
-              >Perfil</b-dropdown-item
-            >
-            <b-dropdown-item href="/logout"
-              ><i class="fa-solid fa-right-from-bracket mr-2"></i>Tancar
-              sessió</b-dropdown-item
-            >
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>-->
     <div id="miNavBar">
         <b-navbar
             toggleable="lg"
@@ -220,18 +162,8 @@ export default {
     background: #E55490;
     border-top-right-radius: 5px;
     border-top-left-radius: 5px;
-   /* animation-name: anim;
-    animation-duration: 6s;
-    animation-iteration-count: infinite;*/
 }
-/*
-@keyframes anim{
-    0% {background-color: rgb(255, 0, 234);}
-   25% {background-color: rgb(191, 102, 172);}
-   50% {background-color: rgb(196, 87, 138);}
-   75% {background-color: rgb(191, 102, 172);}
-  100% {background-color: rgb(255, 0, 234);}
-}*/
+
 
 .router-link-exact-active {
     font-weight: bold;
