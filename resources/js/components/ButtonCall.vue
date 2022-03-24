055d90df3a1e41ce2ff690c9171fa9729eda14c0
santiago-examen <77068843+smarescat@users.noreply.github.com>
2022-03-24 10:54:33 +0100

    form call

--- resources/js/components/ButtonCall.vue
+++ resources/js/components/ButtonCall.vue
@@ -1,46 +1,80 @@
 <template>
-  <div>
-    <nav class="menu">
-      <input
-        type="checkbox"
-        v-model="checkboxValue"
-        class="menu-open"
-        name="menu-open"
-        id="menu-open"
-      />
+  <div class="col-12 d-flex">
+        <div class="col-8">
+        <b-card class="" :class="checkboxValue ? 'show-card' : 'hide-card'">
+            <b-card-body>
+            <nav class="menu">
+                <input
+                type="checkbox"
+                v-model="checkboxValue"
+                class="menu-open"
+                name="menu-open"
+                id="menu-open"
+                />
 
-      <label class="menu-open-button" for="menu-open" id="text-call" @click="!checkboxValue ? start() : stop()">
-        <i :class="'fas ' + (!checkboxValue ? 'fa-phone mr-1' : 'fa-phone-slash mr-1')" ></i>
-        {{ !checkboxValue ? "Iniciar trucada"  : "Finalitzar trucada" }}
-      </label>
-      <div class="form-floating user-select-none menu-item">
-        <b-form-input
-          class="input-phone"
-          type="number"
-          name=""
-          id="input-phone"
-          placeholder="Nº telèfon"
+                <label
+                class="menu-open-button"
+                for="menu-open"
+                id="text-call"
+                @click="!checkboxValue ? start() : stop()"
+                >
+                <i
+                    :class="
+                    'fas ' +
+                    (!checkboxValue ? 'fa-phone mr-1' : 'fa-phone-slash mr-1')
+                    "
+                ></i>
+                {{ !checkboxValue ? "Iniciar trucada" : "Finalitzar trucada" }}
+                </label>
+                <div class="form-floating user-select-none menu-item">
+                <b-form-input
+                    class="input-phone"
+                    type="number"
+                    name=""
+                    id="input-phone"
+                    placeholder="Nº telèfon"
+                />
+                <label class="user-select-none" for="input-phone"
+                    >Nº telèfon</label
+                >
+                </div>
+                <div class="form-floating user-select-none menu-item m-top">
+                <p class="m4">{{ time }}</p>
+                </div>
+            </nav>
+            <div class="row">
+                <div class="col">
+                <form-call v-show="checkboxValue == true"></form-call>
+                </div>
+            </div>
+            </b-card-body>
+        </b-card>
+        </div>
 
-        />
-        <label class="user-select-none" for="input-phone"
-          >Nº telèfon</label
-        >
-      </div>
-      <div class="form-floating user-select-none menu-item m-top">
-        <p class="m4">{{ time }}</p>
-      </div>
-    </nav>
+        <div class="col-4">
+            <b-card class="" :class="checkboxValue ? 'show-card' : 'hide-card'">
+                <expedients-call v-show="checkboxValue == true"></expedients-call>
+            </b-card>
+        </div>
   </div>
 </template>
 
 <script>
+import FormCall from "./FormCall.vue";
+import ExpedientsCall from "./ExpedientsCall.vue";
+
 export default {
+  components: {
+    FormCall,
+    ExpedientsCall,
+  },
+
   mounted() {},
   data() {
     return {
       checkboxValue: false,
       crono: true,
-      time: '00:00:00.000',
+      time: "00:00:00.000",
       running: false,
       started: null,
       timeBegan: null,
@@ -49,62 +83,66 @@ export default {
     };
   },
   methods: {
-      start() {
-        document.querySelector('.input-phone').focus();
+    start() {
+      document.querySelector(".input-phone").focus();
 
-        if(this.running) return;
+      if (this.running) return;
 
-        if(this.timeBegan === null) {
-            this.reset();
-            this.timeBegan = new Date();
-        }
+      if (this.timeBegan === null) {
+        this.reset();
+        this.timeBegan = new Date();
+      }
 
-        if (this.timeStopped !== null) {
-        this.stoppedDuration += (new Date() - this.timeStopped);
-        }
-        this.started = setInterval(this.clockRunning, 10);
-        this.running = true;
+      if (this.timeStopped !== null) {
+        this.stoppedDuration += new Date() - this.timeStopped;
+      }
+      this.started = setInterval(this.clockRunning, 10);
+      this.running = true;
     },
-     stop() {
-        this.running = false;
-        this.timeStopped = new Date();
-        clearInterval(this.started);
-        this.timeBegan = null;
-     },
-
-     reset() {
-        this.running = false;
-        clearInterval(this.started);
-        this.stoppedDuration = 0;
-        this.timeBegan = null;
-        this.timeStopped = null;
-        this.time = "00:00:00.000";
+    stop() {
+      this.running = false;
+      this.timeStopped = new Date();
+      clearInterval(this.started);
+      this.timeBegan = null;
     },
 
-    clockRunning(){
-    var currentTime = new Date()
-    , timeElapsed = new Date(currentTime - this.timeBegan - this.stoppedDuration)
-    , hour = timeElapsed.getUTCHours()
-    , min = timeElapsed.getUTCMinutes()
-    , sec = timeElapsed.getUTCSeconds()
-    , ms = timeElapsed.getUTCMilliseconds();
+    reset() {
+      this.running = false;
+      clearInterval(this.started);
+      this.stoppedDuration = 0;
+      this.timeBegan = null;
+      this.timeStopped = null;
+      this.time = "00:00:00.000";
+    },
 
-    this.time =
-        this.zeroPrefix(hour, 2) + ":" +
-        this.zeroPrefix(min, 2) + ":" +
-        this.zeroPrefix(sec, 2) + "." +
+    clockRunning() {
+      var currentTime = new Date(),
+        timeElapsed = new Date(
+          currentTime - this.timeBegan - this.stoppedDuration
+        ),
+        hour = timeElapsed.getUTCHours(),
+        min = timeElapsed.getUTCMinutes(),
+        sec = timeElapsed.getUTCSeconds(),
+        ms = timeElapsed.getUTCMilliseconds();
+
+      this.time =
+        this.zeroPrefix(hour, 2) +
+        ":" +
+        this.zeroPrefix(min, 2) +
+        ":" +
+        this.zeroPrefix(sec, 2) +
+        "." +
         this.zeroPrefix(ms, 3);
     },
 
     zeroPrefix(num, digit) {
-    var zero = '';
-    for(var i = 0; i < digit; i++) {
-        zero += '0';
-    }
-    return (zero + num).slice(-digit);
-    }
-
-  }
+      var zero = "";
+      for (var i = 0; i < digit; i++) {
+        zero += "0";
+      }
+      return (zero + num).slice(-digit);
+    },
+  },
 };
 </script>
 
@@ -113,7 +151,10 @@ a {
   color: inherit;
 }
 
-h1,h2,h3,h4 {
+h1,
+h2,
+h3,
+h4 {
   margin: 0;
   margin-bottom: 10px;
   margin-top: 10px;
@@ -125,7 +166,7 @@ h1 {
 
 .menu {
   filter: url("#shadowed-goo");
-  margin-left: 50px;
+  height: 80px;
 }
 
 .menu-item {
@@ -142,14 +183,14 @@ h1 {
   transition: transform ease-out 200ms;
 }
 .menu-open-button {
-  background: #12e266;
+  background: #218838;
   border-radius: 5%;
   font-weight: bold;
-  padding: 5px;
+  padding: 12px;
   padding-inline: 30px;
   position: absolute;
   top: 20px;
-  color: black;
+  color: white;
   text-align: center;
   transform: translate3d(0, 0, 0);
   transition: transform ease-out 200ms;
@@ -210,13 +251,32 @@ h1 {
 }
 .input-phone::-webkit-outer-spin-button,
 .input-phone::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
+  -webkit-appearance: none;
+  margin: 0;
 }
 .m4 {
   margin: 4px;
 }
 .m-top {
-    margin-top: 3px;
+  margin-top: 3px;
+}
+
+.hide-card {
+  background-color: transparent;
+  border: 0;
+  box-shadow: 0px;
+}
+
+.show-card {
+  background-color: white;
+  border: 1px;
+  box-shadow: 0px 5px 25px 0px rgb(0 0 0 / 20%);
+}
+
+.w-card {
+  width: 60%;
+}
+.w-card2 {
+    width: 40%;
 }
 </style>
---
+++ resources/js/components/FormCall.vue
@@ -0,0 +1,181 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <b-tabs content-class="mt-3 " class="m-top-20">
+        <!-- TAB Nota comuna -->
+        <b-tab title="Nota comuna" active>
+          <div class="col-12 center p-0">
+            <div class="form-floating user-select-none">
+              <b-form-input id="name" type="text" placeholder="Nom" required />
+              <label class="user-select-none" for="input-name"> Nom </label>
+            </div>
+
+            <div class="form-floating user-select-none">
+              <b-form-input
+                id="surname"
+                type="text"
+                placeholder="Cognom"
+                required
+              />
+              <label class="user-select-none" for="input-surname">
+                Cognom
+              </label>
+            </div>
+
+            <div class="form-floating user-select-none">
+              <b-form-input
+                id="origin"
+                type="text"
+                placeholder="Nacionalitat"
+                required
+              />
+              <label class="user-select-none" for="input-origin">
+                Nacionalitat
+              </label>
+            </div>
+
+            <div class="form-floating user-select-none">
+              <b-form-input
+                id="relation"
+                type="text"
+                placeholder="Relació amb l'incident"
+                required
+              />
+              <label class="user-select-none" for="input-relation">
+                Relació amb l'incident
+              </label>
+            </div>
+          </div>
+          <div class="col-12 p-0">
+                <div class="form-floating user-select-none">
+                    <b-form-textarea
+                        id="description"
+                        class="mt-3 h-150"
+                        type="text"
+                        placeholder="Relació incident"
+                        required
+                        rows="5"
+                    />
+                    <label class="user-select-none" for="input-relation">
+                        Descripció incident
+                    </label>
+                </div>
+          </div>
+        </b-tab>
+        <!-- TAB Identificació de la trucada -->
+        <b-tab title="Identificació de la trucada">
+            <div class="row center">
+                <div>
+                    <b-form-select
+                    v-model="selected"
+                    :options="options"
+                    size="sm"
+                    class="mt-3"></b-form-select>
+                </div>
+                <div class="form-floating user-select-none">
+                    <b-form-input
+                        id="comarca"
+                        type="text"
+                        placeholder="Comarca"
+                        required
+                    />
+                    <label class="user-select-none" for="input-comarca">
+                        Comarca
+                    </label>
+                </div>
+                <div class="form-floating user-select-none">
+                    <b-form-input
+                        id="provincia"
+                        type="text"
+                        placeholder="Provincia"
+                        required
+                    />
+                    <label class="user-select-none" for="input-provincia">
+                        Provincia
+                    </label>
+                </div>
+            </div>
+
+            <div class="row center mt-3">
+                <div>
+                    <b-form-select
+                    v-model="selected"
+                    :options="optionsType"
+                    size="sm"
+                    class="mt-3"></b-form-select>
+                </div>
+                <div class="form-floating user-select-none">
+                    <b-form-input
+                        id="address"
+                        type="text"
+                        placeholder="Adreça"
+                        required
+                    />
+                    <label class="user-select-none" for="input-address">
+                        Adreça
+                    </label>
+                </div>
+            </div>
+
+            <div class="col-12 p-0 mt-3">
+                <div class="form-floating user-select-none">
+                    <b-form-textarea
+                        id="information"
+                        class="mt-3 h-150"
+                        type="text"
+                        placeholder="Informació rellevant"
+                        required
+                        rows="5"
+                    />
+                    <label class="user-select-none" for="input-information">
+                        Informació rellevant
+                    </label>
+                </div>
+          </div>
+        </b-tab>
+        <!-- TAB Localització de l'emergència -->
+        <b-tab title="Localització de l'emergència"> </b-tab>
+        <!-- TAB Tipificació de l'emergència -->
+        <b-tab title="Tipificació de l'emergència"> </b-tab>
+      </b-tabs>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        selected: null,
+        options: [
+          { value: null, text: 'Municipio' },
+        ],
+        optionsType: [
+            { value: null, text: 'Tipo' }
+        ]
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style scoped>
+.form-floating > .form-control,
+.form-floating > .form-select {
+    height: 40px;
+}
+
+.m-top-20 {
+  margin-top: 20px;
+}
+.h-150 {
+  height: 150px !important;
+}
+.center {
+  display: flex;
+  justify-content: space-between;
+}
+.p-0 {
+  padding: 0px;
+}
+</style>
