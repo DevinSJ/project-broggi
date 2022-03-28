beeb617ad997ecd3504cc6dbb0e7d5c01b12cec1
santiago-examen <77068843+smarescat@users.noreply.github.com>
2022-03-28 12:42:07 +0200

    in progress

---
+++ app/Http/Controllers/Api/TipusLocalitzacions.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Tipus_localitzacions;
+use Illuminate\Http\Request;
+
+class TipusLocalitzacions extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tipus_localitzacions  $tipus_localitzacions
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tipus_localitzacions $tipus_localitzacions)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tipus_localitzacions  $tipus_localitzacions
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tipus_localitzacions $tipus_localitzacions)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tipus_localitzacions  $tipus_localitzacions
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tipus_localitzacions $tipus_localitzacions)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UsuarisController;
 use App\Http\Controllers\Api\AjudesFrasesController;
+use App\Http\Controllers\Api\ComarquesController;
 use App\Http\Controllers\Api\ExpedientsController;
+use App\Http\Controllers\Api\MunicipisController;
+use App\Http\Controllers\Api\ProvinciesController;
+use App\Http\Controllers\Api\TipusLocalitzacions;
+use App\Http\Controllers\Api\TrucadesController;
+use App\Http\Controllers\TipusLocalitzacionsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +26,8 @@ use App\Http\Controllers\Api\ExpedientsController;
 Route::apiResource('users', UsuarisController::class);
 Route::apiResource('help-phrases', AjudesFrasesController::class);
 Route::apiResource('expedients', ExpedientsController::class);
+Route::apiResource('call', TrucadesController::class);
+Route::apiResource('provinces', ProvinciesController::class);
+Route::apiResource('regions', ComarquesController::class);
+Route::apiResource('towns', MunicipisController::class);
+Route::apiResource('LocationsTypes', TipusLocalitzacionsController::class);
